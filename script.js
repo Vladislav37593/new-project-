271e340393b0517b79e6c717bd5530b31450f543
Noar <fld.noar@gmail.com>
2022-01-23 13:45:11 +0300

    Fix menu

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function myFunction(x) {
     x.classList.toggle("change");
 }
 
+let gamburgerMenu = document.querySelector('.gamburger_menu'),
+    horizontalMenu = document.querySelector('.topnav');
+
 function openMenu(gamb, menu) {
-    gamb.addEventListener("click")
+    gamb.addEventListener("click", function(event) {
+        event.preventDefault();
+        menu.classList.toggle('topnav--hide');
+    });
 }
+
+openMenu(gamburgerMenu, horizontalMenu);
