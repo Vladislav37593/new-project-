d0efa57c67486f53d3a397510d2c6f7b095a7a1c
Vladislav <kva6238805@gmail.com>
2022-01-29 19:11:05 +0300

    delet gamburger/new menu

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+let linksGroup = document.querySelectorAll(".tablink");
+
+for (let button of linksGroup) {
+    button.addEventListener('click', openCity())
 }
 
-let gamburgerMenu = document.querySelector('.gamburger_menu'),
-    horizontalMenu = document.querySelector('.topnav');
+function openCity(cityName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-function openMenu(gamb, menu) {
-    gamb.addEventListener("click", function(event) {
-        event.preventDefault();
-        menu.classList.toggle('topnav--hide');
-    });
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    // Show the specific tab content
+    document.getElementById(cityName).style.display = "block";
+
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
 }
 
-openMenu(gamburgerMenu, horizontalMenu);
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
