885706a9cf274c5bedd12b4fa81dc3f0d61815f1
Vladislav <kva6238805@gmail.com>
2022-02-05 19:44:15 +0300

    new functions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ for (let button of linksGroup) {
     button.addEventListener('click', openCity())
 }
 
-function openCity(cityName, elmnt, color) {
+function openCity(cityName, elmnt, image) {
     // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -15,14 +15,14 @@ function openCity(cityName, elmnt, color) {
     // Remove the background color of all tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.backgroundImage = "";
     }
 
     // Show the specific tab content
     document.getElementById(cityName).style.display = "block";
 
     // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
+    document.getElementById(cityName).style.backgroundImage = image;
 }
 
 // Get the element with id="defaultOpen" and click on it
@@ -32,18 +32,3 @@ document.getElementById("defaultOpen").click();
 
 
 
-
-// Get the container element
-var btnContainer = document.getElementById("myDIV");
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
